7428929ba6ca659ca0c028ee63d5c3afda7f2725
hirokitakeuchi <hiroki.takeuchi@gmail.com>
2011-09-20 16:38:56 -0700

    Edited lib/premailer-rails3/hook.rb via GitHub

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -1,29 +1,35 @@
 module PremailerRails
   class Hook
     def self.delivering_email(message)
-      # If mail has a text part then preserve it
       
+      # Convert html part of the message
+      
+      puts "before everything -"
+      puts message.html_part.body.to_s
+      puts message.text_part.body.to_s
+      
+      html_part = Premailer.new(message.html_part.body.to_s)
+      
+      puts "html_part"
+      puts html_part.inspect
+      
+      # If mail has a text part then preserve it, else convert html to text
       if message.text_part
         text_part = message.text_part.body.to_s
-      end
-      # If the mail only has one part, it may be stored in message.body. In that
-      # case, if the mail content type is text/html, the body part will be the
-      # html body.
-      if message.html_part
-        html_body = message.html_part.body.to_s
-      elsif message.content_type =~ /text\/html/
-        html_body = message.body.to_s
+      else
+        html_part = premailer.to_plain_text
       end
       
+      # Set charset of the message
+      charset = message.charset
+      
+      # Delete existing message
       message.body = nil
-
-      premailer = Premailer.new(html_body)
-      charset   = message.charset
-      text_part = premailer.to_plain_text unless text_part
       
+      # Add back html and text parts to the message
       message.html_part do
         content_type "text/html; charset=#{charset}"
-        body premailer.to_inline_css
+        body html_part.to_inline_css
       end
 
       message.text_part do
